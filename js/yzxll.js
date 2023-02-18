5635557453df2061f28afa4bb9b42e8398ca5db2
一只小流浪 <109851917+YiZhiXiaoLiuLang@users.noreply.github.com>
2023-02-18 21:04:41 +0800

    Create yzxll.js

---
+++ js/yzxll.js
@@ -0,0 +1,8 @@
+console.log(
+  '%c%s%c%s',
+  'color:red;font-size:24px;',
+  '   一只小流浪\n',
+  'color:blue;font-weight:bold;',
+  '\n　　　　　　＿＿＿\n　　　　　／＞　　フ\n　　　　　|　_　_l\n　　　　／`ミ＿?ノ\n　　　/　　　　|\n　　　/　ヽ　　\\\n　　│　　|　|　|\n　／￣|　　|　|　|\n　|(￣ヽ＿_ヽ_)__)\n　＼二つ\n',
+);
+console.log("话说Intel有没有女朋友？（无恶意）");
